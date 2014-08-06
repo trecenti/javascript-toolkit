0144d90db1b397d3a0b225abc901e0ff36f9e59a
Bruno Tavares <bruno.exz@gmail.com>
2014-08-06 15:35:29 -0300

    Create project outputs

--- build.js
+++ build.js
@@ -1,12 +1,6 @@
 var Metalsmith = require('metalsmith');
 var render = require('consolidate').handlebars;
 
-var json = {
-  categories: [
-    { "title" : "Banana", "categories": [{ "title": "updated" }] }
-  ]
-};
-
 var index = function(files, metalsmith, done) {
   var metadata = metalsmith.metadata();
 
@@ -17,10 +11,53 @@ var index = function(files, metalsmith, done) {
   });
 };
 
-Metalsmith(__dirname)
-  .destination('./build')
-  .metadata(json)
-  .use(index)
-  .build(function (error, files ) {
-    if (error) { console.log(arguments); }
+var project = function(files, metalsmith, done) {
+  var metadata = metalsmith.metadata();
+
+  Object.keys(metadata.categories).forEach(function (category) {
+    Object.keys(metadata.categories[category]).forEach(function (subcategory) {
+      Object.keys(metadata.categories[category][subcategory]).forEach(function (project) {
+        var entry = metadata.categories[category][subcategory][project];
+        var output = category.replace(' ','-') + '/' + subcategory.replace(' ','-') + '/' + project + '.html';
+
+        render('templates/project.html', entry, function (error, content) {
+          if (error) { done(error); }
+          files[output] = { contents: content };
+          done();
+        });
+      });
+    });
   });
+};
+
+var convertToMetadata = function (files, metalsmith, done) {
+  var metadata = metalsmith.metadata();
+  metadata.categories = metadata.categories || {};
+
+  Object.keys(files).forEach(function (filePath) {
+    var content = files[filePath].contents.toString();
+    var fileData = JSON.parse(content);
+
+    var tags = filePath.split('/');
+    var category = tags[0].replace('-',' ');
+    var subcategory = tags[1].replace('-', ' ');
+    var entry = tags[2].split('.')[0];
+
+    metadata.categories[category] = metadata.categories[category] || {};
+    metadata.categories[category][subcategory] = metadata.categories[category][subcategory] || {};
+    metadata.categories[category][subcategory][entry] = fileData;
+
+    delete files[filePath];
+  });
+
+  done();
+};
+
+Metalsmith(__dirname)
+.destination('./build')
+.use(convertToMetadata)
+.use(index)
+.use(project)
+.build(function (error, files ) {
+  if (error) { console.log(arguments); }
+});
